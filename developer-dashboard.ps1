6dbcf71703137587b3ed6d936f4dd62f03a8a0c5
unknown <developer@SP2013DEV.spdev.local>
2014-04-25 11:25:26 -0700

    Add developer dashboard activating script

---
+++ developer-dashboard.ps1
@@ -0,0 +1,10 @@
+﻿$d = [Microsoft.SharePoint.Administration.SPWebService]::ContentService.DeveloperDashboardSettings;
+#sp2013
+$d.DisplayLevel = "On";
+
+#sp2007 - sp2010
+#$d.DisplayLevel = "OnDemand";
+
+$d.RequiredPermissions = "EmptyMask";
+$d.TraceEnabled = $true;
+$d.Update();
